6a0b039c05d747a9fc9e57f3cabcbfc30042da96
Danny Nguyen <danny@keeb.io>
2022-02-22 09:09:14 -0500

    Initial version of longest sub sequence

---
+++ longest-sub-seq.fsx
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S dotnet fsi
+
+let longestSubSeq s =
+    let longestRun s =
+        let rec loop longest acc a =
+            match a with
+            | [] -> max longest acc
+            | 1 :: tail -> loop longest (acc + 1) tail
+            | _ :: tail -> loop (max longest acc) 0 tail
+
+        loop 0 0 s
+
+    s
+    |> List.sort
+    |> List.pairwise
+    |> List.map (fun (a, b) -> b - a)
+    |> longestRun
+    |> (+) 1
+
+let a1 = [ 1; 9; 87; 3; 10; 4; 20; 2; 45 ]
+
+let a2 =
+    [ 36
+      41
+      56
+      35
+      91
+      33
+      34
+      92
+      43
+      37
+      42 ]
+
+printfn "%A" <| longestSubSeq a1
+printfn "%A" <| longestSubSeq a2
+printfn "%A" <| longestSubSeq [ 5; 7 ]
